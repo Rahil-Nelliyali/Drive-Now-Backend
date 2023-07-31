fd79c6fccda5a909ed0eba42582243365a0c4ddc
Rahil-Nelliyali <rahilnelliyali0@gmail.com>
2023-07-31 12:19:38 +0530

    smtp

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -223,9 +223,8 @@ CORS_ALLOW_HEADERS = [
     "x-requested-with",
 ]
 #smtp
-
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT= 587
-EMAIL_HOST_USER="drivenowsmtp@gmail.com"
-EMAIL_HOST_PASSWORD="robgnbcklnasmnje"
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER=config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS=True
