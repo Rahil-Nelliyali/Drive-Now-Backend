96abead20d7181a6b943959ae3fee60744d1d1cd
Rahil-Nelliyali <rahilnelliyali0@gmail.com>
2023-08-23 12:31:45 +0530

    cors origin fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,12 +86,12 @@ SIMPLE_JWT = {
 
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.middleware.security.SecurityMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
